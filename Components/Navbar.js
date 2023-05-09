e5cccf839ca8dcbb2d2837ccf7aa84bf6c5cc35f
Pranati Tikku <pranatitikku@gmail.com>
2023-05-09 10:47:57 +0530

    text-normal

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 const Navbar = () => {
-    return ( <nav className='w-full bg-white text-black border-gray-200 dark:bg-gray-900'>
+    return ( <nav className='w-full bg-white border-gray-200 dark:bg-gray-900'>
         <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
             <h1>Logo</h1>
             <Link href='/'>Home</Link>
